fc6dc915bc2b1be25eaf48defc858582e786c656
D-Raj-Grg <racingfoxs@gmail.com>
2026-01-27 23:16:15 +0545

    Initial Commit

---
+++ gymtrackpromax/gymtrackpromaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymtrackpromaxApp.swift
+//  gymtrackpromax
+//
+//  Created by D Raj Gurung on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct gymtrackpromaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
